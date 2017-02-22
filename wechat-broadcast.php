1fd0261904dd41d11be25b4557eca7f1ca68584a
Jiang Kun <chinese.jk@gmail.com>
2017-02-23 02:15:35 +0800

    update version

--- wechat-broadcast.php
+++ wechat-broadcast.php
@@ -3,7 +3,7 @@
   Plugin Name: 微信推送图文消息
   Plugin URI: https://wordpress.org/plugins/wechat-broadcast
   Description: 在发布文章时推送图文消息到公众号的订阅用户
-  Version: 1.1.2
+  Version: 1.2.0
   Author: JaQuan
   Author URI: http://80to.me
   License: GPL2
